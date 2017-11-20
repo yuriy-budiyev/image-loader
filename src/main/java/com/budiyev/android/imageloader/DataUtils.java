fbecc7edbd5bd230daf51c76caa04a8d7d180fc0
Yuriy Budiyev <y.budiyev@corp.mail.ru>
2017-11-20 12:20:50 +0300

    Dev

--- src/main/java/com/budiyev/android/imageloader/DataUtils.java
+++ src/main/java/com/budiyev/android/imageloader/DataUtils.java
@@ -60,6 +60,15 @@ public final class DataUtils {
     }
 
     /**
+     * Default {@link DataDescriptor}, {@code data}'s toString() method will be used
+     * for key generation, any characters are allowed
+     */
+    @NonNull
+    public static <T> DataDescriptor<T> descriptor(@NonNull T data) {
+        return new StringDataDescriptor<>(data, null);
+    }
+
+    /**
      * Generate SHA-256 hash string with {@link Character#MAX_RADIX} radix
      * for specified {@link String}; usable for keys of {@link DataDescriptor} implementations
      *
